e1983de3d1c0a3f6deb25da3de9af15c1972dc88
Maxi Bora <maxi.borachoc@gmail.com>
2021-03-06 20:02:38 -0300

    navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const NavBar = () => {
+    return (<div> 
+      <h3>Repuestos Bora</h3>
+      <ul>
+          <li> <a href=""> Impulsores de arranque</a></li>
+      </ul>
+    </div>
+    );
+};
