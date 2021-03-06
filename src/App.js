bf6de0166c740dbf8b289ca6a2563905dcec481f
Maxi Bora <maxi.borachoc@gmail.com>
2021-03-06 20:04:32 -0300

    otro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import {NavBar} from './components/navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ¡Te damos la bienvenida a Repuestos Borachoc!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         Ponete cómodo y elegí tranquilo.
-        </a>
-      </header>
+      <NavBar/>
     </div>
   );
 }
