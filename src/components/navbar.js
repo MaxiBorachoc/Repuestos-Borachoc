5bcf0e00ae93ba4fab41edc208d0077d220221e2
Maxi Bora <maxi.borachoc@gmail.com>
2021-03-10 20:09:47 -0300

    lista

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,8 +4,16 @@ export const NavBar = () => {
     return (<div> 
       <h3>Repuestos Bora</h3>
       <ul>
-          <li> <a href="https://www.zensa.com.br/produtos/impulsores-de-arranque/1"> Impulsores de arranque</a></li>
+          <li> <a href="https://www.zensa.com.br/produtos/impulsores-de-arranque/1"> Impulsores</a></li>
           <li><a href="https://listado.mercadolibre.com.ar/regulador-de-voltaje"> Reguladores de voltaje</a></li>
+          <li><a href="">Rulemanes</a></li>
+          <li><a href="">Colectores</a></li>
+          <li><a href="">Inducidos</a></li>
+          <li><a href="">Portaescobillas</a></li>
+          <li><a href="">Bujes</a></li>
+          <li><a href="">Lámparas</a></li>
+          <li><a href="">Arranques</a></li>
+          <li><a href="">Alternadores</a></li>
           
       </ul>
     </div>
